c3cb8a4823fa18f4865f933af80b79240ebf0186
Mayumi Kusunoki <162358019+MayumiKusunoki@users.noreply.github.com>
2024-05-02 16:41:20 -0300

    Adiciona as seguintes funções:

--- js/app.js
+++ js/app.js
@@ -4,19 +4,36 @@ limpar();
 // document.getElementById('valor-total').textContent = 'R$ 0';
 
 function adicionar() {
-    // recuperar valores: nome do produto, quantidade e valor
+    
+  // recuperar valores: nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+
     let nomeProduto = produto.split('-')[0];
     let valorUnitario =  produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value;
-    // calcular o preço, o nosso subtotal
+   
+  //verificar se o produto selecionado é válido
+   if (!produto || produto.trim() === "") {
+    alert("Selecione um produto válido.");
+    return;
+  }
+
+  // verificar se a quantidade inserida é válida
+  if (isNaN(quantidade) || quantidade <=0) {
+    alert("Insira uma quantidade válida.");
+    return;
+  }
+ 
+  // calcular o preço, o nosso subtotal
     let preco = quantidade * valorUnitario;
     let carrinho = document.getElementById('lista-produtos');
-    // adicionar no carrinho
+  
+  // adicionar no carrinho
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
   </section>`;
-    // atualizar o valor total da compra
+   
+  // atualizar o valor total da compra
     totalGeral = totalGeral + preco;
     let campoTotal = document.getElementById('valor-total');
     campoTotal.textContent = `R$ ${totalGeral}`;
